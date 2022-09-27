92a52f30caff5c38410d1a23bcdf97910cdb432f
Lara Puelman <100933327+LaraPuelman@users.noreply.github.com>
2022-09-27 16:06:13 +0000

    Cambio

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, jsonify, request, session, redirect, u
 import sqlite3
 app = Flask(__name__)
 app.secret_key = 'clave-secreta'
-
 #--------------------- Iniciar sesión ---------------------#
 @app.route('/')
 def index():
@@ -60,64 +59,58 @@ def crearUsuario():
 # ------------------------ Tableros ----------------------------#   
 @app.route('/inicio')
 def inicio():
-  
-  if session['nombre'] != "":
-    return render_template('index.html')
-  else:
+  try:
+    usuario= session['nombre'] 
+  except KeyError:
     return redirect(url_for('index'))
+  else:
+    return render_template('index.html', nombre=usuario)
   
 @app.route('/tablero/<n>')
 def tablero(n):
-  tab = int(n)-1
-  
-  conn = sqlite3.connect('memotest.db')
-  conn.row_factory = sqlite3.Row
-  cur = conn.cursor()
-  cur.execute(f""" SELECT *
-  FROM Tableros
-  WHERE idTablero={tab}
-  ORDER BY numeroColumna, numeroFila; """)  
-  va = cur.fetchall()
-  lista=[]
-  for i in va:
-      li = {
-           "id": i[0],
-          "idTablero": i[1],
-          "numeroColumna": i[2],
-          "numeroFila": i[3],
-          "valor": i[4]
-      }
-      lista.append(li)
-  print(lista)
-  cur.execute(f"""SELECT COUNT(DISTINCT numeroColumna)
-  FROM Tableros
-  WHERE idTablero={tab}""")
-  col = cur.fetchone()
-  cantColum = col[0]
-  print(cantColum)
-  cur.execute(f""" SELECT COUNT(DISTINCT numeroFila)
-  FROM Tableros
-  WHERE idTablero={tab}""")
-  fil = cur.fetchone()
-  cantFilas = fil[0]
-  print(cantFilas)
+  try:
+    usuario= session['nombre'] 
+  except KeyError:
+    return redirect(url_for('index'))
+  else:
+    tab = int(n)-1
+    conn = sqlite3.connect('memotest.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute(f""" SELECT *
+    FROM Tableros
+    WHERE idTablero={tab}
+    ORDER BY numeroColumna, numeroFila; """)  
+    va = cur.fetchall()
+    if va != []:
+      lista=[]
+      for i in va:
+          li = {
+               "id": i[0],
+              "idTablero": i[1],
+              "numeroColumna": i[2],
+              "numeroFila": i[3],
+              "valor": i[4]
+          }
+          lista.append(li)
+      print(lista)
+      cur.execute(f"""SELECT COUNT(DISTINCT numeroColumna)
+      FROM Tableros
+      WHERE idTablero={tab}""")
+      col = cur.fetchone()
+      cantColum = col[0]
+      print(cantColum)
+      cur.execute(f""" SELECT COUNT(DISTINCT numeroFila)
+      FROM Tableros
+      WHERE idTablero={tab}""")
+      fil = cur.fetchone()
+      cantFilas = fil[0]
+      print(cantFilas)
+    
+      return render_template("tableros.html", tab=tab,lista=lista, cantFilas=cantFilas, cantColum=cantColum)
+    else:
+      return redirect(url_for("inicio"))
+      
 
-  return render_template("tableros.html", tab=tab,lista=lista, cantFilas=cantFilas, cantColum=cantColum)
-  
-
-@app.route("/test")
-def test():
-#  id = int
-  conn = sqlite3.connect('memotest.db')
-  cur = conn.cursor()
-  q= """ SELECT COUNT(DISTINCT numeroColumna)
-  FROM Tableros
-  WHERE idTablero=0
-"""
-  cur.execute(q)
-  fil = cur.fetchall()
-  print(fil)
-#  cantco = fil[0]
-  return f"hola {fil}"
 # ---------------------------------------------------------------#
 app.run(host='0.0.0.0', port=81)
